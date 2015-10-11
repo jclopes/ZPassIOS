c7e5ea7c5bc5acf2f8b3320e20ecea1900038b2b
João Lopes <jc.lopes@gmail.com>
2015-10-11 18:41:08 +0200

    Draft algorithm for generating password.

--- ZPassIOS/EncodingModel.swift
+++ ZPassIOS/EncodingModel.swift
@@ -28,6 +28,24 @@ class EncodingModel {
 }
 
 func genPass(salt: String, input: String, pass: String) -> String {
-    let prehash = (salt+"@"+input+":"+pass as NSString).dataUsingEncoding(NSUTF8StringEncoding)
-    return prehash!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+    let prehash = salt+"@"+input+":"+pass
+    let str = prehash.cStringUsingEncoding(NSUTF8StringEncoding)
+    let strLen = Int32(prehash.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+    let digestLenInt: Int = Int(CC_SHA256_DIGEST_LENGTH)
+    
+    let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLenInt)
+    
+    CC_SHA256(str!, UInt32(bitPattern: strLen), result)
+   
+    let digest = stringFromResult(result, length: digestLenInt)
+    result.dealloc(digestLenInt)
+
+    return digest
+}
+
+private func stringFromResult(result: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
+    let bla = NSMutableData()
+    bla.appendBytes(result, length: length)
+    let bla64 = bla.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+    return String(bla64)
 }
