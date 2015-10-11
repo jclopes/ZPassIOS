a59917fda4b2ee422606b1c74cba0c95ea5983d7
João Lopes <jc.lopes@gmail.com>
2015-10-11 18:39:06 +0200

    xcode project stuff

---
+++ ZPassIOS/ZPassIOS-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ZPassIOS-Bridging-Header.h
+//  ZPassIOS
+//
+//  Created by João Lopes on 2015/10/04.
+//  Copyright © 2015 João Lopes. All rights reserved.
+//
+
+#ifndef ZPassIOS_Bridging_Header_h
+#define ZPassIOS_Bridging_Header_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* ZPassIOS_Bridging_Header_h */
